1a77e77c042c9708f745d5fd951c3cfcb87b6a97
alexey.nosov <alexey.nosov@softservice.org>
2022-07-03 14:57:18 +0700

    fixed log

--- src/main/java/com/automafia/automafia/Game/GameTasks.java
+++ src/main/java/com/automafia/automafia/Game/GameTasks.java
@@ -80,7 +80,7 @@ public class GameTasks {
 
             if (usersMovedByGame.get(gameId) == null) continue;
             log.info("Users moved now: " + usersMovedByGame.get(gameId).size()+  ". Round ID: " + gameInfo.getCurrentRound().getId());
-            log.info("Night users %s", gameInfo.getGame().getCountNightUsers());
+            log.info("Night users " + gameInfo.getGame().getCountNightUsers());
             log.info("Users move: " + usersMovedByGame.get(gameId));
             if (usersMovedByGame.get(gameId).size() >= gameInfo.getGame().getCountNightUsers()) {
                 Game gameWithNewRound = gameService.nextRound(gameId);
